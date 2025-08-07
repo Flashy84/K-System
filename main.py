87663ef57548403e9dee7753445717677f5487ba
Chris-Stian Flashy <32557957+Flashy84@users.noreply.github.com>
2025-08-07 09:39:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ def print_ticket(number):
     buf += FEED_TOP
     buf += "            |\_/|        D\___/\\n".encode('cp865')
     buf += "            (0_0)         (0_o)\n".encode('cp865')
-    buf += "           ==(Y)==         (V)\n".encode('cp865')
+    buf += "       ==(Y)==         (V)\n".encode('cp865')
     buf += "----------(u)---(u)----oOo--U--oOo---\n".encode('cp865')
     buf += "__|_______|_______|_______|_______|___\n".encode('cp865')
 
