4468a1046f513353c5c9a6e773309842edc387d3
Chris-Stian Flashy <32557957+Flashy84@users.noreply.github.com>
2025-08-07 08:02:26 +0200

    Update main.py
    lagt til en bounce_time=0.3 på knappen for å unngå at den registrerer flere trykk ved ett enkelt trykk

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ def issue_new_ticket():
 
 def main():
     # Oppsett knapp
-    btn = Button(BUTTON_PIN)
+    btn = Button(BUTTON_PIN, bounce_time=0.3)  # Debounce knapp for å unngå flere utskrifter per trykk
     btn.when_pressed = issue_new_ticket
 
     print("Starter Epson TM-T88VI kiosk (en lapp per trykk)...")
