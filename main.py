cd7c2f08aa2f8742d7244ec07b7be01a0de5883d
Chris-Stian Flashy <32557957+Flashy84@users.noreply.github.com>
2025-08-09 09:42:51 +0200

    Update main.py
    Changed IP  adress  to printer

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from queue import Queue
 from gpiozero import Button
 
 # ----------------- KONFIGURASJON -----------------
-IP_ADDRESS      = "192.168.10.154"
+IP_ADDRESS      = "192.168.10.106"
 PORT            = 9100
 API_ENDPOINT    = 'https://www.chris-stian.no/kundeskjerm/create_queue.php'
 STATUS_ENDPOINT = 'https://www.chris-stian.no/kundeskjerm/status.php'
